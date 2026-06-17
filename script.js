c52dab0ee682e860aab920571cdffbce89f0408b
Bayzid <dbmbayzid00@gmail.com>
2026-06-17 22:24:01 +0600

    first theory added

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-// PART A: THEORY QUESTIONS
-// 1. Execution Context
+
+
+// Write a program that prints "Hello World" after 3 seconds.
+setTimeout(()=>{
+    console.log("Hello World");
+},3000)
+
+
+// Create a digital clock using setInterval().
+
+
+/* */
+/* */
+/* */
+
+
+
